8e1ab95a090ce835dee0b28f7556f70bf4e6f57f
"AndriySV" <"A.S.Elips@gmail.com">
2014-09-06 16:14:49 +0300

    change

---
+++ src/main/java/com/softserveinc/softtour/Main.java
@@ -0,0 +1,24 @@
+package com.softserveinc.softtour;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.softserveinc.softtour.dao.UserDao;
+
+public class Main {
+
+	private ApplicationContext context;
+	
+	public static void main(String[] args) {
+		Main ob = new Main();
+		ob.createContext();
+	}
+
+	private void createContext() {
+		context = new ClassPathXmlApplicationContext("spring/config.xml");
+		
+		UserDao userDao = (UserDao) context.getBean("userDao");
+		
+	}
+	
+}
