11650328205594990e920b62bfcafdd50ed3cfce
AndriySV <A.S.Elips@gmail.com>
2014-09-06 15:41:47 +0300

    change

--- src/main/java/com/softserveinc/softtour/dao/Impl/FavoriteDaoImpl.java
+++ src/main/java/com/softserveinc/softtour/dao/Impl/FavoriteDaoImpl.java
@@ -10,62 +10,105 @@ import com.softserveinc.softtour.entity.Favorite;
 import com.softserveinc.softtour.entity.Tour;
 import com.softserveinc.softtour.entity.User;
 
+/**
+ * @author Andriy
+ *	Contains the methods for work with table Favorite in the SoftTour database 
+ */
 public class FavoriteDaoImpl extends HibernateDaoSupport implements FavoriteDao {
 
+	/**
+	 * Saves the object favorite to the table Favorite
+	 */
 	@Override
 	public void save(Date date, User user, Tour tour) {
 		Favorite favorite = new Favorite(date, user, tour);
 		getHibernateTemplate().save(favorite);
 	}
 
+	/**
+	 *  Updates the object favorite with the specified id
+	 *  id - id of the object favorite which will updated
+	 */
 	@Override
 	public void update(long id, Date date, User user, Tour tour) {
-		Favorite favorite = (Favorite) getHibernateTemplate().get(Favorite.class, id);
-		
+		Favorite favorite = (Favorite) getHibernateTemplate().get(
+				Favorite.class, id);
+
 		if (favorite != null) {
 			favorite.setDate(date);
 			favorite.setUser(user);
 			favorite.setTour(tour);
-			
+
 			getHibernateTemplate().update(favorite);
 		} else {
-			System.err.println("Error ! \n Favorite is null !");
+			System.err.println("Error ! \n No favorite with this ID ! ");
 		}
 	}
-	
+
+	/**
+	 *  Deletes the object favorite with the specified id
+	 *  id - id of the object favorite which will deleted
+	 */
 	@Override
 	public void delete(long id) {
-		Favorite favorite = (Favorite) getHibernateTemplate().get(Favorite.class, id);
-		
+		Favorite favorite = (Favorite) getHibernateTemplate().get(
+				Favorite.class, id);
+
 		if (favorite != null) {
 			getHibernateTemplate().delete(favorite);
 		} else {
-			System.err.println("Error ! \n Favorite is null !");
+			System.err.println("Error ! \n No favorite with this ID ! ");
 		}
 	}
 
+	/**
+	 *  Returns the object favorite with the specified id
+	 *  id - id of the object favorite which will returned
+	 */
 	@Override
 	public Favorite findById(long id) {
-		Favorite favorite = (Favorite) getHibernateTemplate().get(Favorite.class, id);
+		Favorite favorite = (Favorite) getHibernateTemplate().get(
+				Favorite.class, id);
+
 		return favorite;
 	}
 
-
-    @Override
-    public List<Favorite> findByUser(User user) {
-        @SuppressWarnings("unchecked")
-		List<Favorite> list = (List<Favorite>) getHibernateTemplate().find("FROM Favorite WHERE user_id = ?", user.getId());
-        return list;
-    }
-
-
-    @Override
-	public List<Favorite> getAll() {
+	/**
+	 *  Returns the list of the objects favorite which contain the specified object user 
+	 */
+	@Override
+	public List<Favorite> findByUser(User user) {
+		String queryFindByUser = "FROM Favorite WHERE user_id = ?";
 		@SuppressWarnings("unchecked")
-		List<Favorite> list = getHibernateTemplate().find("From Favorite");
+		List<Favorite> list = (List<Favorite>) getHibernateTemplate().find(
+				queryFindByUser, user.getId());
+
+		return list;
+	}
+
+	/**
+	 *  Returns the list of the objects favorite which contain the specified object tour 
+	 */
+	@Override
+	public List<Favorite> findByTour(Tour tour) {
+		String queryFindByTour = "FROM Favorite WHERE tour_id = ?";
+		@SuppressWarnings("unchecked")
+		List<Favorite> list = (List<Favorite>) getHibernateTemplate().find(
+				queryFindByTour, tour.getId());
 		
 		return list;
 	}
 
+	/**
+	 *  Returns the list of all objects favorite which are contained in the table Favorite
+	 */
+	@Override
+	public List<Favorite> getAll() {
+		String queryGetAll = "From Favorite";
+		@SuppressWarnings("unchecked")
+		List<Favorite> list = getHibernateTemplate().find(queryGetAll);
+
+		return list;
+	}
 
 }
--- src/main/java/com/softserveinc/softtour/dao/Impl/UserDaoImpl.java
+++ src/main/java/com/softserveinc/softtour/dao/Impl/UserDaoImpl.java
@@ -10,9 +10,15 @@ import com.softserveinc.softtour.entity.Role;
 import com.softserveinc.softtour.entity.User;
 import com.softserveinc.softtour.entity.template.Sex;
 
+/**
+ * @author Andriy
+ * 	Contains the methods for work with table User in the SoftTour database
+ */
 public class UserDaoImpl extends HibernateDaoSupport implements UserDao {
-
 	
+	/**
+	 * Saves the object user to the table User
+	 */
 	@Override
 	public void save(String name, String email, String password, Date birthday,
 			byte age, Sex sex, String phone, Role role) {
@@ -20,6 +26,10 @@ public class UserDaoImpl extends HibernateDaoSupport implements UserDao {
 		getHibernateTemplate().save(user);
 	}
 
+	/**
+	 *  Updates the object user with the specified id
+	 *  id - id of the object user which will updated
+	 */
 	@Override
 	public void update(long id, String name, String email, String password,
 			Date birthday, byte age, Sex sex, String phone, Role role) {
@@ -38,30 +48,67 @@ public class UserDaoImpl extends HibernateDaoSupport implements UserDao {
 			
 			getHibernateTemplate().update(user);
 		} else {
-			System.err.println("Error ! \n User is null !");
+			System.err.println("Error ! \n No user with this ID !");
 		}
 	}
 
+	/**
+	 *  Deletes the object user with the specified id
+	 *  id - id of the object user which will deleted
+	 */
 	@Override
 	public void delete(long id) {
 		User user = (User) getHibernateTemplate().get(User.class, id);
 		if (user != null) {
 			getHibernateTemplate().delete(user);
 		} else {
-			System.err.println("Error ! \n User is null !");
+			System.err.println("Error ! \n No user with this ID !");
 		}
 	}
 
+	/**
+	 *  Returns the object user with the specified id
+	 *  id - id of the object user which will returned
+	 */
 	@Override
 	public User findById(long id) {
 		User user = (User) getHibernateTemplate().get(User.class, id);
+		
 		return user;
 	}
+	
+	/**
+	 *  Returns list of the objects user with the specified name
+	 */
+	@Override
+	public List<User> findByName(String name) {
+		String queryFindByName = "From User Where name = ?"; 
+		@SuppressWarnings("unchecked")
+		List<User> list = getHibernateTemplate().find(queryFindByName, name);
+		
+		return list;
+	}
+	
+	/**
+	 *  Returns the list of the objects user which contain the specified object role
+	 */
+	@Override
+	public List<User> findByRole(Role role) {
+		String queryFindByRole = "From User Where role_id = ?"; 
+		@SuppressWarnings("unchecked")
+		List<User> list = getHibernateTemplate().find(queryFindByRole, role.getId());
+		
+		return list;
+	}
 
+	/**
+	 *  Returns the list of all objects user which are contained in the table User
+	 */
 	@Override
 	public List<User> getAll() {
+		String queryGetAll = "From User";
 		@SuppressWarnings("unchecked")
-		List<User> list = getHibernateTemplate().find("From User");
+		List<User> list = getHibernateTemplate().find(queryGetAll);
 		
 		return list;
 	}
