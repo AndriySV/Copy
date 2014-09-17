72ae0c6649b852855d9515aceec672b405319f7e
"AndriySV" <"A.S.Elips@gmail.com">
2014-09-17 19:37:45 +0300

    all

--- src/main/java/com/softserveinc/softtour/service/Impl/RoleServiceImpl.java
+++ src/main/java/com/softserveinc/softtour/service/Impl/RoleServiceImpl.java
@@ -7,16 +7,8 @@ import com.softserveinc.softtour.service.RoleService;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
 public class RoleServiceImpl implements RoleService {
-
-	@Autowired
-	private RoleDao roleDao;
+    private RoleDao roleDao;
 
     public void setRoleDao (RoleDao roleDao){this.roleDao = roleDao;}
     @Override
@@ -26,7 +18,6 @@ public class RoleServiceImpl implements RoleService {
     @Override
     public void delete(long id){roleDao.delete(id);}
     @Override
-    @Transactional(propagation=Propagation.REQUIRED)
     public Role findById(long id){return roleDao.findById(id);}
     @Override
     public List<Role> findByName(String name){return roleDao.findByName(name);}
--- src/main/java/com/softserveinc/softtour/service/Impl/UserServiceImpl.java
+++ src/main/java/com/softserveinc/softtour/service/Impl/UserServiceImpl.java
@@ -3,8 +3,6 @@ package com.softserveinc.softtour.service.Impl;
 import java.sql.Date;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,12 +17,18 @@ import com.softserveinc.softtour.service.UserService;
  * 	Contains the methods for work with table User in the SoftTour database
  *  Supports a transaction
  */
-@Service
 @Transactional(propagation=Propagation.SUPPORTS, readOnly = true)
 public class UserServiceImpl implements UserService {
 
-	@Autowired
 	private UserDao userDao;
+
+	/**
+	 * Sets the userDao object
+	 * @param userDao - object of the class UserDaoImpl
+	 */
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
 
 	/**
 	 * Saves the object user to the table User
